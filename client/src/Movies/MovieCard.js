3fbf6a01b32612aaf7c4331db4a8fbf39abbab86
April Ma <54913304+april5622@users.noreply.github.com>
2020-03-17 18:24:21 -0700

    delete function working

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 
 const MovieCard = props => {
-  const { title, director, metascore, stars } = props.movie;
+  const { title, director, metascore, stars , id} = props.movie;
+  
+
+  const deleteMovie = e => {
+    e.preventDefault();
+
+    axios 
+      .delete(`http://localhost:5000/api/movies/${id}`)
+      .then(res => {
+        props.updateMovies(res.data);
+        console.log("going to push to new url");
+        props.history.push("/");
+        console.log("past push to new url")
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="movie-card">
       <h2>{title}</h2>
@@ -18,6 +37,8 @@ const MovieCard = props => {
           {star}
         </div>
       ))}
+
+      <button onClick={deleteMovie}>Delete</button>
     </div>
   );
 };
---
+++ client/src/components/UpdateForm.js
@@ -0,0 +1,101 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+const initialItem = {
+    title: "",
+    director: "",
+    metascore:"",
+    stars:""
+};
+
+const UpdateForm = props => {
+    const [movie, setMovie] = useState(initialItem);
+
+    useEffect(() => {
+        const movieToUpdate = props.movies.find(movie => {
+            return `${movie.id}` === props.match.params.id;
+        });
+
+        console.log("movieToUpdate", movieToUpdate);
+
+        if(movieToUpdate) {
+            setMovie(movieToUpdate);
+        }
+    }, [props.movies, props.match.params.id]);
+
+    const changeHandler = e => {
+        e.persist();
+        let value = e.target.value;
+        if (e.target.name === "price") {
+            value = parseInt(value, 10);
+        }
+
+        setMovie({
+            ...movie,
+            [e.target.name]: value
+        });
+    };
+
+    const handleSubmit = e => {
+        e.prevent.Default();
+
+        axios
+            .put(`http://localhost:5000/api/movies/${movie.id}`, movie)
+            .then(res => {
+                console.log(res);
+                props.updateMovies(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }; // handleSubmit
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type ="text"
+                name="title"
+                onChange={changeHandler}
+                placeholder="Title"
+                value={movie.name}
+                />
+                <div className="baseline" />
+
+                <input 
+                type="text"
+                name="director"
+                onChange={changeHandler}
+                placeholder="Director"
+                value={movie.director}
+                />
+                <div className="baseline" />
+
+                <input 
+                type="text"
+                name="metascore"
+                onChange={changeHandler}
+                placeholder="Metascore"
+                value={movie.metascore}
+                />
+                <div className="baseline" />
+
+                <input 
+                type="text"
+                name="stars"
+                onChange={changeHandler}
+                placeholder="Stars"
+                value={movie.stars}
+                />
+                <div className="baseline" />
+
+                <button type="submit">Update</button>
+            </form>
+        </div>
+    )
+
+
+
+} // end of UpdateForm
+
+export default UpdateForm;
